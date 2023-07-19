cef0793fe422b101b6ea7bf778ae1eb8632ab9be
rich.sugiarto@gmail.com <rich.sugiarto@gmail.com>
2023-07-19 12:58:23 +0800

    Fix: Update education section with MUI #time 10m

--- src/pages/Education.js
+++ src/pages/Education.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Row, Col, Container, Image } from 'react-bootstrap';
 import './css/Education.css';
+import { Container, Grid } from '@mui/material';
+
 function Education() {
     return (
         <>
             <div className="myeducation">
-                <Container>
+                <Container max-width="xl">
                     <h2
                         style={{
                             color: '#555',
@@ -18,8 +19,8 @@ function Education() {
                         Education
                     </h2>
                     <div style={{ border: '1px solid #e73131', width: '15%', margin: '0 auto' }}></div>
-                    <Row style={{ paddingTop: '10vh' }}>
-                        <Col lg="6" sm="12">
+                    <Grid container style={{ paddingTop: '10vh' }} spacing={6}>
+                        <Grid item md={6} xs={12}>
                             <span style={{ textAlign: 'justify' }}>
                                 <h3>Master of Information Management</h3>
                                 <p>National Taiwan University of Science and Technology, Taipei, Taiwan</p>
@@ -30,8 +31,8 @@ function Education() {
                                     Proposed a similarity mechanism to improve accuracy in distinguishing motions.
                                 </p>
                             </span>
-                        </Col>
-                        <Col lg="6" sm="12">
+                        </Grid>
+                        <Grid item md={6} xs={12}>
                             <span style={{ textAlign: 'justify' }}>
                                 <h3>Bachelor of Science</h3>
                                 <p>Institut Sains dan Teknologi Terpadu Surabaya (Formerly Sekolah Tinggi Teknik Surabaya)</p>
@@ -47,8 +48,8 @@ function Education() {
                                 </p>
                                 <p>The Best Undergraduate Student at Informatics Technology Department iSTTS, Surabaya, Indonesia, 2019.</p>
                             </span>
-                        </Col>
-                    </Row>
+                        </Grid>
+                    </Grid>
                 </Container>
             </div>
         </>
